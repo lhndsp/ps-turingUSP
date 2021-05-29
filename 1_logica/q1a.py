b2932afe931f343f02e42b14068890c9f3107672
lhnds <lhnds97@gmail.com>
2021-05-29 20:22:33 -0300

    adicionando comentarios na q1a

--- 1_logica/q1a.py
+++ 1_logica/q1a.py
@@ -8,15 +8,19 @@
 
 def comparacao(emails: list, k: int):
     """comparacao da entrada atual com a penultima, e contagem
-    dos caracteres ue podem ser economizados entre ambas
+       dos caracteres ue podem ser economizados entre ambas, a ideia é: 
+       percorrer todos os caracteres da entrada atual e comparalos com o de mesmo indice
+       da entrada k-1 e se forem iguais incrementa a variavel n(zerada toda vez que essa
+       funcao é chamada) ate que tenha um caracter divergende, dai sai do loop e retorna 
+       a variavel n, que é acrescentada a lista comparacao.
 
     Parametros:
         - emails (list): lista com dois elementos: o email atual e o penultimo 
-        - k (int): indicie atual da lista
+        - k (int): indice atual da lista
     Saida:
-        (int): numero de caracteres economizados na comparacao dos dois email 
+        (int): numero de caracteres economizados na comparacao entre o email atual e o penultimo
     """
-    # Elimina o dominio @usp.br
+    # Elimina o dominio @usp.br que nao vai ser economizado
     emails = [i.replace('@usp.br', '') for i in emails]
 
     # transforma a string em uma lista de caracteres, equivalende ao str.split() *N sabia se podia usar
@@ -43,8 +47,13 @@ def main():
     economia = []
     for i in range(lenMails):
         mailList.append(input())
+        
+        # para cada emai vai retornar a o numero de caracateres economizados
+        # dai depois é so fazer o print do sum(lista) que tem a economia total
+        # entre todos os email digitados
         economia.append(comparacao(emails=mailList, k=i))
     print(sum(economia))
 
+    
 if __name__ == '__main__':
     main()
