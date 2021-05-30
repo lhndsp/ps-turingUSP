1ed702c358288974c540b5a7b17820e1dbd74292
lhnds <lhnds97@gmail.com>
2021-05-30 12:19:48 -0300

    q2b comentada

--- 1_logica/q2b.py
+++ 1_logica/q2b.py
@@ -11,43 +11,71 @@
 ## acho que consigui explicar melhor nela
 
 class Fila(object):
-    """ 1) Cria uma lista ´estacionamento´ de zeros de tamanha ´tam´
-        2) cria uma lista ´fila´ de zeos de tamanho ´interesse´
-        3) para cada instante t na entrada ´instantes´, substitui na lista ´estacionamento´ no indicie
-           t - 1 (para normalizar com a indexacao da lista) o valor do index do 
-           respectivo t
-        4) 
-    """
     def __init__(self, tam, instantes, interesse):
-        self.estacionamento = [0]*tam
-        self.instantes = instantes
+        # cria uma lista representando estacinamento, por default vazio (com 0s)
+        self.estacionamento = [0]*tam 
+        
+        # a lista com os instantes digitados na entrada, sendo que o 
+        # index + 1 da lista representa o id do carro
+        self.instantes = instantes 
+        
+        # o instante aser observado
         self.final = interesse
+        
+        # cria a fila de carros, a principio com 0s a srem substituidos depois
+        # se o instante de interesse for maior que o numero de carros a entrar
+        # representado por ´tam´ cria uma lista de tamanho ´tam´ pois mesmo
+        # depois de acabarem os carros as observacoes continuam
+        # se for menor entao cria lista do tamanho de instantes
         if max(instantes) < interesse:
             self.fila = [0]*interesse 
         else:
             self.fila = [0]*max(instantes)
 
+            
     def preencheFila(self):
+        """ a fila de carros por default é preenchida de 0, e cada index representa o instante
+            e o valor do element no index o id do carro, essa funcao vai alterar na fila de 
+            carros nos respectivos indexes referentes aos digitados na entrada, com o id do 
+            resectivo carro
+        """
         for j, i in enumerate(self.instantes):
             self.fila[i-1] = j+1
         print(self.fila)
         
+        
     def mover(self):
+        """ usando o exemplo 2 do enunciado)
+            com a fila de carros = [0, 0, 1, 5, 0, 2, 0, 0, 4, 0, 0, 3, 0, 0, 6, 7]
+            o estacionameto estaria de seguinte forma do instante 1 ate o instante
+            de interesse:
+            [0, 0, 0, 0, 0] {instante 1}
+            [0, 0, 0, 0, 0] {instante 2}
+            [1, 0, 0, 0, 0] {instante 3}
+            [5, 1, 0, 0, 0] {instante 4}
+            [0, 5, 1, 0, 0] {instante 5}
+            [2, 0, 5, 1, 0] {instante 6}
+            [0, 2, 0, 5, 1] {instante 7}
+        """
+        
         k = 0
         while k < self.final:
             n = self.fila.pop()
             self.estacionamento.insert(0, n)
             self.estacionamento.pop()
             k += 1
+            
+            
     #def __repr__(self):
     #    return self.estacionamento
+    
     
     def gerenciaFila(self):
         self.preencheFila()
         self.fila = self.fila[::-1]
-        print(self.fila)
         self.mover()
         return self.estacionamento
+        
         
 def main():
     # tamanho do estacionamento e numero de carros
